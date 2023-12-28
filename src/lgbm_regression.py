07bd824bfc6f7c8f982071cf0a8474a75579de39
ishii <ishiken4869@gmail.com>
2023-12-28 19:21:39 +0900

    logPの実験を実行

--- src/lgbm_regression.py
+++ src/lgbm_regression.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 TRAIN = "../data/smiles_all_property.csv"
-TARGET_COL = "HOMO/LUMO gap"
+TARGET_COL = "logP"
 SMILES_COL = "smiles"
 PROPERTY = TARGET_COL
 
@@ -80,12 +80,12 @@ def main():
 
     params_dict = {
             #'boosting_type': ['rf'],
-            'num_leaves': [80],
+            'num_leaves': [2],
             'criterion': ['absolute_error'],
-            'max_depth': [100],
-            'min_child_samples': [60],
+            'max_depth': [120],
+            'min_child_samples': [170],
             'learning_rate': [0.01],
-            'n_estimators': [32000],
+            'n_estimators': [1000000],
             'reg_lambda': [0],
             'colsample_bytree': [0.4],
             'n_jobs': [multiprocessing.cpu_count()],
